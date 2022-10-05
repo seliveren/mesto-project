af6f159c170f3b02232457eaaad1d87165950f60
Yulia Selivanova <selivanyulia@mail.ru>
2022-10-05 10:34:20 +0300

    fix: add changes for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: 'https://seliveren.github.io/mesto-project/',
+    publicPath: '/src/',
   },
   mode: 'development',
   devServer: {
